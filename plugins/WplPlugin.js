98322bebe47e70978f6e64b5e176f63a4705652a
marder <iwashier@gmail.com>
2017-09-05 00:10:17 +0200

    Update WplPlugin.js

--- plugins/WplPlugin.js
+++ plugins/WplPlugin.js
@@ -2,6 +2,7 @@
 
     var fs = require("fs-extra");
     var path = require("path");
+    var xml2js = require('xml2js');
 
     function WplPlugin() {
         return {
@@ -13,12 +14,41 @@
 
                     let songs = [];
                     
-                    // ToDo parse wpl
+                    // Example for a ".wpl" file
+                    // <?wpl version="1.0" ?>
+                    // <smil>
+                    //    <head>
+                    //       <title>Musik</title>
+                    //    </head>
+                    //    <body>
+                    //        <seq>
+                    //           <media src="./relative/path/to/file.mp3" />
+                    //           ...
+                    //        </seq>
+                    //    </body>
+                    // </smil>
+                                        
+                    fs.readFile(file).then(function (content) {
+                        
+                        xml2js.parseString(content, function (err, result) {
+                            if (err) {
+                                reject(err);
+                                return;
+                            } else {
+                                
+                                var songs = [];
+                                
+                                // ToDo - parse result
+                                // ToDo - use other plugins to check <media> files
 
-                    resolve({
-                        type: 'playlist',
-                        name: path.basename(file),
-                        songs: songs
+                                resolve({
+                                    type: 'playlist',
+                                    name: path.basename(file),
+                                    songs: songs
+                                });                                   
+                            }                            
+                        });
+                        
                     });
                 });
 
